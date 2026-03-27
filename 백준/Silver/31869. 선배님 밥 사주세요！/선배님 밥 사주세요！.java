4064cfa3952fd0e4276789e316fb802a36a8abcd
박현우 <73339681+Dustsw@users.noreply.github.com>
2026-03-27 12:35:06 +0900

    [Silver III] Title: 선배님 밥 사주세요!, Time: 104 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Silver/31869. 선배님 밥 사주세요！/선배님 밥 사주세요！.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class plan{
+        String name;
+        int week;
+        int day;
+        int price;
+        public plan(String name, int week, int day, int price) {
+            this.name = name;
+            this.week = week;
+            this.day = day;
+            this.price = price;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N =  Integer.parseInt(br.readLine());
+
+        Map<String, plan> plans = new HashMap<>();
+        StringTokenizer st;
+        for(int i  = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int week = Integer.parseInt(st.nextToken());
+            int day = Integer.parseInt(st.nextToken());
+            int price = Integer.parseInt(st.nextToken());
+            plans.put(name, new plan(name, week, day, price));
+        }
+
+
+        for(int i  = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int money = Integer.parseInt(st.nextToken());
+
+            if(plans.get(name).price > money ){
+                plans.remove(name);
+            }
+        }
+
+        boolean[][] visited = new boolean[11][7];
+        for(String name: plans.keySet()){
+            visited[plans.get(name).week][plans.get(name).day] = true;
+        }
+
+        int cnt = 0;
+        int max = 0;
+
+        for(int i  = 1; i <= 10; i++){
+            for(int j  = 0; j < 7; j++){
+                if(visited[i][j]){
+                    cnt++;
+                    max = Math.max(max, cnt);
+                }
+                else{
+                    cnt = 0;
+                }
+            }
+        }
+        System.out.println(max);
+
+
+    }
+}
