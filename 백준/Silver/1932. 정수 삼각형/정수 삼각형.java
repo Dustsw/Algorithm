1def46b94794c3f6189a2039b48c431fc7acd0fa
박현우 <73339681+Dustsw@users.noreply.github.com>
2026-04-22 13:54:13 +0900

    [Silver I] Title: 정수 삼각형, Time: 244 ms, Memory: 25716 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N =  Integer.parseInt(br.readLine());
+        
+        int[][] dp =  new int[N][N];
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j <= i; j++) {
+                dp[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i = N-2; i >= 0; i--) {
+            for(int j = 0; j <= i; j++) {
+                dp[i][j] += Math.max(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        System.out.println(dp[0][0]);
+    }
+}
